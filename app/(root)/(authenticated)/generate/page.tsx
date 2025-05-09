73ec303ef96255b3bfa5d4f93446920f644a6b1e
Dani3lx <1danielxu@gmail.com>
2025-05-09 15:19:07 -0400

    Generate page responsive

--- app/(root)/(authenticated)/generate/page.tsx
+++ app/(root)/(authenticated)/generate/page.tsx
@@ -1,22 +1,30 @@
 import GenerateForm from "@/components/form/GenerateForm";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import React from "react";
 
 const page = () => {
   return (
-    <div className="flex items-center mt-24 w-3/5 gap-8">
-      <div className="w-1/2 flex flex-col gap-8">
-        <div className="flex flex-col gap-3">
-          <h1 className="text-4xl font-bold tracking-tight">
+    <div className="flex flex-col container px-8 gap-8">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-4xl font-bold tracking-tight">
             Generate an interview
-          </h1>
-          <p className="text-muted-foreground">
+          </CardTitle>
+          <CardDescription className="text-muted-foreground">
             Customize your interview by answering a few quick questions.
-          </p>
-        </div>
-
-        <GenerateForm />
-      </div>
-      <div className="h-full w-1/2 bg-gray-100 rounded-3xl"></div>
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid md:grid-cols-2 gap-8">
+          <GenerateForm />
+          <div className="h-full bg-gray-100 rounded-3xl max-md:hidden"></div>
+        </CardContent>
+      </Card>
     </div>
   );
 };
