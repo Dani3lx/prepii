13fe062b8556c76355b108bad27f8de242db8d02
Dani3lx <1danielxu@gmail.com>
2025-05-22 01:36:37 -0400

    updated env variable name

--- lib/redis.ts
+++ lib/redis.ts
@@ -2,7 +2,7 @@ import { createClient } from "redis";
 
 const redisClient = createClient({
   username: process.env.NEXT_PUBLIC_REDIS_USERNAME,
-  password: process.env.NEXT_PUBLIC_REDIS_PASSWORD,
+  password: process.env.NEXT_REDIS_PASSWORD,
   socket: {
     host: process.env.NEXT_PUBLIC_REDIS_HOST!,
     port: Number(process.env.NEXT_PUBLIC_REDIS_PORT),
